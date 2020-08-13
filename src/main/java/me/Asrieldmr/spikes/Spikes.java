d858de91283e0b6fae1124e62383489ad8d9b3e2
berg <berg@192.168.0.107>
2020-08-13 17:08:24 +0300

    hope it works

--- src/main/java/me/Asrieldmr/spikes/Spikes.java
+++ src/main/java/me/Asrieldmr/spikes/Spikes.java
@@ -33,7 +33,7 @@ public class Spikes extends JavaPlugin implements SlimefunAddon {
         ItemStack categoryItem = new CustomItem(Material.HEAVY_WEIGHTED_PRESSURE_PLATE, "&eSpikes", "", "&a> Click to open");
 
        
-        NamespacedKey categoryId = new NamespacedKey(this, "spikes");
+        NamespacedKey categoryId = new NamespacedKey(this, "SPIKES");
         Category category = new Category(categoryId, categoryItem);
 
         
@@ -45,6 +45,16 @@ public class Spikes extends JavaPlugin implements SlimefunAddon {
       
         SlimefunItem item = new SlimefunItem(category, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
         item.register(this);
+        
+        SlimefunItemStack slimefunItem1 = new SlimefunItemStack("Wooden_Spike", Material.OAK_PRESSURE_PLATE, "&Wooden Spike", "&aThe Basic Spike - almost kill everyone who steps on it (half a heart left)");
+
+        
+        ItemStack[] recipe1 = { null , null, null , null, new ItemStack(Material.WOODEN_SWORD), null, new ItemStack(Material.WOODEN_SWORD), new ItemStack(Material.OAK_LOG), new ItemStack(Material.WOODEN_SWORD) };
+
+      
+        SlimefunItem item1 = new SlimefunItem(category, slimefunItem1, RecipeType.ENHANCED_CRAFTING_TABLE, recipe1);
+        item1.register(this);
+        
     }
 
     @Override
@@ -64,3 +74,6 @@ public class Spikes extends JavaPlugin implements SlimefunAddon {
     }
 
 }
+
+
+
